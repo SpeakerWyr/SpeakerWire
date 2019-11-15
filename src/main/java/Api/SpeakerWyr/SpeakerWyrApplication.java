33f0f5421d68f27674d25308eba4ff9c3cdb3c9e
Luke Brannigan <lukebrannigan57@gmail.com>
2019-11-15 15:44:17 -0500

    first push, git ignore, build.gradle, spring

---
+++ src/main/java/Api/SpeakerWyr/SpeakerWyrApplication.java
@@ -0,0 +1,13 @@
+package Api.SpeakerWyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakerWyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakerWyrApplication.class, args);
+	}
+
+}
