bde951f2636ed7137ee35866a3714c58fdaf54fe
BarbaraTaylor <btaylor1787@gmail.com>
2019-11-21 14:41:03 -0500

    Created Speaker.java and Host.java

--- src/main/java/Api/SpeakerWyr/Host.java
+++ src/main/java/Api/SpeakerWyr/Host.java
@@ -1,5 +1,49 @@
 package Api.SpeakerWyr;
 
+import java.util.List;
+
 public class Host {
+	
+	private Long id;
+	private String name;
+	private String location;
+	private String bio;
+	private String headshotUrl;
+	private List<Event> events;
+	
+	public void Host() {}
+	
+	public void Host(String name, String location, String bio, String headshotUrl) {
+		this.name = name;
+		this.location = location;
+		this.bio = bio;
+		this.headshotUrl = headshotUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public String getHeadshotUrl() {
+		return headshotUrl;
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+	
+	
 
 }
--- src/main/java/Api/SpeakerWyr/Speaker.java
+++ src/main/java/Api/SpeakerWyr/Speaker.java
@@ -1,5 +1,51 @@
 package Api.SpeakerWyr;
 
+import java.util.List;
+
 public class Speaker {
+	
+	private Long id;
+	private String name;
+	private String location;
+	private String bio;
+	private String headshotUrl;
+	private List<Talk> talks;
+
+	public void Speaker() {}
+	
+	public void Speaker(String name, String location, String bio, String headshotUrl) {
+		this.name = name;
+		this.location = location;
+		this.bio = bio;
+		this.headshotUrl = headshotUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public String getBio() {
+		return bio;
+	}
+
+	public String getHeadshotUrl() {
+		return headshotUrl;
+	}
+
+	public List<Talk> getTalks() {
+		return talks;
+	}
+	
+	
+	
+	
 
 }
