76bfac30e5e991d3634b70bd0c4996e1bc122953
Brendin Headlee <Brendin.Headlee24@gmail.com>
2019-11-24 14:41:36 -0500

    added to populator

--- src/main/java/Populator.java
+++ src/main/java/Populator.java
@@ -28,11 +28,15 @@ public class Populator implements CommandLineRunner {
 		speakerService.addSpeaker(speakerTalksAlot);
 
 		
+		Talk testTalk1 = new Talk("APIs", "The basics to APIs", Duration.LONG, Linda Luikas);
+		Talk testTalk2 = new Talk("Java", "Its like rosetta stone", Duration.SHORT, Jimmy John;
+		Talk testTalk3 = new Talk("Speaking with confidence", "Let your voice be heard, speak with confidence", Duration.LIGHTNING, Talks Alot);
+		
+		
+		
+		
 		}
 
 }
 
-//		Talk testTalk1 = new Talk("TalkTitle1", "TalkDescription1", Duration.MID, speaker1);
-//		Talk testTalk2 = new Talk("TalkTitle2", "TalkDescription2", Duration.MID, speaker1);
-//		Talk testTalk3 = new Talk("TalkTitle3", "TalkDescription3", Duration.MID, speaker1);
-//		
+		
