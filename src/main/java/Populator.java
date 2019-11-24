fa9ad7bd3490a620f0c39c2be44f7d287b5c9f5a
BarbaraTaylor <btaylor1787@gmail.com>
2019-11-24 14:00:56 -0500

    Populator, began it

---
+++ src/main/java/Populator.java
@@ -0,0 +1,37 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import Api.SpeakerWyr.SpeakerService;
+import Api.SpeakerWyr.TalkService;
+import Api.SpeakerWyr.models.Duration;
+import Api.SpeakerWyr.models.Speaker;
+import Api.SpeakerWyr.models.Talk;
+
+@Component
+public class Populator implements CommandLineRunner {
+	
+	@Autowired
+	private TalkService talkService;
+	@Autowired
+	private SpeakerService speakerService;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Speaker speaker1 = new Speaker("SpeakerName", "SpeakerLocation", 
+				"SpeakerBio", "SpeakerHeadshotUrl");
+		
+		
+		
+		
+		Talk testTalk1 = new Talk("TalkTitle1", "TalkDescription1", Duration.MID, speaker1);
+		Talk testTalk2 = new Talk("TalkTitle2", "TalkDescription2", Duration.MID, speaker1);
+		Talk testTalk3 = new Talk("TalkTitle3", "TalkDescription3", Duration.MID, speaker1);
+		
+		
+		
+		
+	}
+
+}
