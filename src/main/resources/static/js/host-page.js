cf8fed7fbf2c508d46684cc8669e677f69dda006
Brian Bross <brian.bross@gmail.com>
2019-12-05 14:47:15 -0500

    filter functionality WORKS

--- src/main/resources/static/js/host-page.js
+++ src/main/resources/static/js/host-page.js
@@ -3,17 +3,18 @@ function getFilterResults() {
     let tagId = document.querySelector(".filter__tag-select").value;
     let durationId = document.querySelector(".filter__duration-select").value;
     let speakerName = document.querySelector(".filter__speakerName").value;
-    const results = document.querySelector(".search-results");
-
-    let fetchUrl = "localhost:8080/filter?genreId=" + genreId + "&tagId=" + tagId + "&durationId=" + durationId + "&speakerName=" + speakerName;
-
-    fetch request (fetchUrl,  
     
-    then (response)=> {
-        results.innerHTML=response.body;
-    
-    console.log(fetchUrl);
+    const resultsGoHere = document.querySelector(".search-results");
 
+    let fetchUrl = "http://localhost:8080/filter?genreId=" + genreId + "&tagId=" + tagId + "&durationId=" + durationId + "&speakerName=" + speakerName;
+        
+    fetch(fetchUrl)
+        .then ((response) => {
+            return response.text();
+        })
+        .then (data => {
+            resultsGoHere.innerHTML=data;
+        })
 
 }
 
@@ -21,4 +22,3 @@ document.getElementById("filterButton").addEventListener("click", () => {
     event.preventDefault();
     getFilterResults();
 });
-
