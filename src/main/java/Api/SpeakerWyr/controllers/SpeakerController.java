6e5ed5d570a72e5939eca6b67fe47e4aba250c24
Brian Bross <brian.bross@gmail.com>
2019-12-06 11:31:19 -0500

    forced a talk into repo

--- src/main/java/Api/SpeakerWyr/controllers/SpeakerController.java
+++ src/main/java/Api/SpeakerWyr/controllers/SpeakerController.java
@@ -29,23 +29,22 @@ import Api.SpeakerWyr.services.TalkService;
 @Controller
 @RequestMapping("/speaker")
 public class SpeakerController {
-	
+
 	@Autowired
 	private SpeakerService speakerService;
 	@Autowired
-	private TagService tagService; 
+	private TagService tagService;
 	@Autowired
 	private GenreService genreService;
 	@Autowired
 	private TalkService talkService;
-	
-	
+
 	@GetMapping("")
 	public String getSpeakers(Model model) {
 		model.addAttribute("speakers", speakerService.fetchSpeakers());
 		return "speaker-page";
 	}
-	
+
 	@GetMapping("/{id}")
 	public String getSingleSpeaker(@PathVariable Long id, Model model) {
 		model.addAttribute("speaker", speakerService.fetchSpeaker(id));
@@ -53,64 +52,61 @@ public class SpeakerController {
 		model.addAttribute("talks", speakerService.getTalksSpeakerHasListed(id));
 		model.addAttribute("genres", genreService.fetchGenres());
 		model.addAttribute("tags", tagService.fetchTags());
-		return "speaker-page";		
+		return "speaker-page";
 	}
-	
-	@PostMapping("/add-speaker") 
+
+	@PostMapping("/add-speaker")
 	public String addSpeaker(String name, String location, String bio, String headshotUrl) {
 		Speaker newSpeaker = new Speaker(name, location, bio, headshotUrl);
-		speakerService.addSpeaker(newSpeaker); //return speakerService.addSpeaker(newSpeaker);
+		speakerService.addSpeaker(newSpeaker); // return speakerService.addSpeaker(newSpeaker);
 		return "redirect: index";
 	}
-	
-	@PatchMapping("/{id}/edit-speaker") //add to host  
+
+	@PatchMapping("/{id}/edit-speaker") // add to host
 	public String editSpeaker(@PathVariable Long id, String name, String location, String bio, String headshotUrl) {
 		Speaker thisSpeaker = speakerService.fetchSpeaker(id);
 		thisSpeaker.setName(name);
 		thisSpeaker.setLocation(location);
 		thisSpeaker.setBio(bio);
 		thisSpeaker.setHeadShotUrl(headshotUrl);
-//		this needs to be a redirect to the speaker-page with id
-		//return speakerService.addSpeaker(thisSpeaker);
+		// this needs to be a redirect to the speaker-page with id
+		// return speakerService.addSpeaker(thisSpeaker);
 		return "redirect:/speaker/" + id;
 	}
-	
+
 	@DeleteMapping("/{id}/remove-speaker")
 	public void removeSpeaker(@PathVariable Long id) {
 		Speaker speaker = speakerService.fetchSpeaker(id);
 		speakerService.removeSpeaker(speaker);
 	}
-	
+
 	@PatchMapping("/{id}/add-headshot")
 	public Speaker addHeadShot(@PathVariable Long id, @RequestBody String headshotUrl) {
 		Speaker speaker = speakerService.fetchSpeaker(id);
 		speaker.setHeadShotUrl(headshotUrl);
 		return speakerService.addSpeaker(speaker);
 	}
-	
+
 	@PostMapping("/{id}/add-talk")
-	public String addTalk(@PathVariable Long id, String title, String description, Duration duration, Long genreId, Long tagId, String iFrame) {
+	public String addTalk(@PathVariable Long id, String title, String description, Integer durationId, Long genreId,
+			Long tagId, String iFrame) {
 		Speaker speaker = speakerService.fetchSpeaker(id);
-		System.out.println(genreId);
-		System.out.println(title);
-		Genre genre = genreService.fetchGenre(genreId);
-		Tag tag = tagService.fetchTag(tagId);
 		List<Genre> genreToAdd = new ArrayList<Genre>();
 		genreToAdd.add(genreService.fetchGenre(genreId));
 		List<Tag> tagToAdd = new ArrayList<Tag>();
 		tagToAdd.add(tagService.fetchTag(tagId));
+
 		
-		Talk talkToAdd = new Talk(title, description, duration, speaker);
+		
+		Talk talkToAdd = new Talk(title, description, Duration.getDurationById(durationId), speaker);
 		talkToAdd.setGenres(genreToAdd);
 		talkToAdd.setTags(tagToAdd);
 		talkToAdd.setIFrame(iFrame);
 		talkService.addTalk(talkToAdd);
 		speaker.getTalks().add(talkToAdd);
-		
 		speakerService.addSpeaker(speaker);
-		//return "speaker-page";
+
 		return "redirect:/speaker/" + id;
 	}
-	
-	
+
 }
--- src/main/java/Api/SpeakerWyr/models/Duration.java
+++ src/main/java/Api/SpeakerWyr/models/Duration.java
@@ -3,18 +3,48 @@ package Api.SpeakerWyr.models;
 
 public enum Duration {
 	
-	LIGHTNING ("15 minutes or less"),
-	SHORT ("15 to 30 minutes"),
-	MID ("30 to 60 minutes"),
-	LONG ("60 minutes or more");
+	LIGHTNING (900, "15 minutes or less"),
+	SHORT (901, "15 to 30 minutes"),
+	MID (902, "30 to 60 minutes"),
+	LONG (903, "60 minutes or more");
 	
-	private final String displayDuration;
+//	private final Long id;
+//	private final String displayDuration;
 	
-	private Duration (String displayDuration) {
+	public String displayDuration;
+	public Integer id;
+	
+	private Duration(Integer id, String displayDuration){
 		this.displayDuration = displayDuration;
+		this.id = id;
 	}
+	
+//	private Duration (String displayDuration) {
+//		this.displayDuration = displayDuration;
+//	}
 	
 	public String getDisplayDuration() {
 		return displayDuration;
 	}
+	
+	public static Duration getDurationById(Integer id) {
+//		switch(id) {
+//		case 900:
+//			return Duration.LIGHTNING;
+//		case 901:
+//			return Duration.SHORT;
+//		case 902:
+//			return Duration.MID;
+//		case 903:
+//			return Duration.LONG;
+//		}
+		return Duration.LONG;
+	}
+	
+	public Integer getDurationId() {
+		return id;
+	}
+	
+	
+	
 }
